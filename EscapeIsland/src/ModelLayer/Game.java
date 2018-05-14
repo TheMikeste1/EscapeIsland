d1df47d368403c7cae11735d77f28c17dc8df7bd
Collin <clarso24@byui.edu>
2018-05-14 13:48:22 -0600

    5/14/18 created java application, added item class from team assignment

---
+++ EscapeIsland/src/ModelLayer/Game.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModelLayer;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author collin
+ */
+public class Game implements Serializable {
+
+    public Game() {
+    }
+    
+    private Item[] item;
+    
+}
