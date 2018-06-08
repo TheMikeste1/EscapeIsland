6d86dcbd30655a61d0fd47ee94037246d0017307
Austi <Austi@LAPTOP-5ITOLKRT>
2018-06-08 13:56:13 -0600

    Merge origin/master
    Conflicts:
    	EscapeIsland/src/ModelLayer/Location.java
    	EscapeIsland/src/ModelLayer/PuzzleLocation.java

--- EscapeIsland/src/byui/cit260/escapeIsland/view/StartProgramView.java
+++ EscapeIsland/src/byui/cit260/escapeIsland/view/StartProgramView.java
@@ -5,6 +5,8 @@
  */
 package byui.cit260.escapeIsland.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author collin
@@ -12,43 +14,64 @@ package byui.cit260.escapeIsland.view;
 public class StartProgramView {
 
     private static String[] getInputs() {
-    System.out.println("**** getInputs() called ****");
-    String[] inputs = new String[1];
-    
+        System.out.println("**** getInputs() called ****");
+        String[] inputs = new String[1];
+
+        inputs = new String[1];
+
         System.out.println("description of view");
-        
-    boolean valid = false;
+
+        boolean valid = false;
         while (valid == false) {
             System.out.println("prompt message");
-            
+            String[] getName = new String[5];
+            Scanner sc = new Scanner(System.in);
+            getName[0] = sc.nextLine();
+            System.out.println(getName[0]);
+
+            String yourName = getName[0].trim();
+            if (yourName.length() < 1) {
+                System.out.println("You must enter a non-blank value");
+
+                continue;
+            }
+            valid = true;
+            inputs[0] = yourName;
         }
-    
-    return inputs;
+
+        return inputs;
     }
 
     private static boolean doAction(String[] inputs) {
         System.out.println("****doAction called ****");
         System.out.println("\tinputs = " + inputs[0]);
-        
+
         return true;
     }
+
     public StartProgramView() {
-        
-    }
-    
-  public static void displayStartProgramView() {
 
-       boolean endOfView = false;
+    }
+
+    public static void displayStartProgramView() {
+
+        boolean endOfView = false;
         do {
-       String[] inputs=getInputs();
-        System.out.println("input name");
-        if (inputs[0].length() < 1 || inputs[0].equals('Q')){ 
-            return;}
-        endOfView = doAction(inputs);
+            String[] inputs = getInputs();
+            System.out.println("input name");
+            if (inputs[0].length() < 1 || inputs[0].equals('Q')) {
+                return;
+            }
+            else if (inputs[0].length() < 1 || inputs[0].equals('q')) {
+                return;
+            
+                
+            }
+            
+               
+            endOfView = doAction(inputs);
         } while (endOfView != true);
-        
-    }
-  
-  
-}
 
+    }
+
+}
