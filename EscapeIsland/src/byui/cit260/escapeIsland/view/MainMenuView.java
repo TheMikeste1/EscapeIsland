aa0bb2f62f24fa0bcb4f020ea1238764a0894a14
Austi <Austi@LAPTOP-5ITOLKRT>
2018-06-11 13:48:53 -0600

    Merge origin/master
    Conflicts:
    	EscapeIsland/src/ModelLayer/Location.java
    	EscapeIsland/src/ModelLayer/PuzzleLocation.java

---
+++ EscapeIsland/src/byui/cit260/escapeIsland/view/MainMenuView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.escapeIsland.view;
+
+/**
+ *
+ * @author Austin
+ */
+public class MainMenuView {
+
+    public void displayMainMenuView() {
+        System.out.println("Display Main Menu View was called.");
+    }
+    
+}
