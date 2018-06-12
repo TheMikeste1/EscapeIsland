f94aa776a4d24881687b2256ed6b356c56405a96
Austi <Austi@LAPTOP-5ITOLKRT>
2018-06-12 12:38:57 -0600

    Merge origin/master
    Conflicts:
    	EscapeIsland/src/ModelLayer/Location.java
    	EscapeIsland/src/ModelLayer/PuzzleLocation.java

--- EscapeIsland/src/byui/cit260/escapeIsland/view/MainMenuView.java
+++ EscapeIsland/src/byui/cit260/escapeIsland/view/MainMenuView.java
@@ -5,6 +5,9 @@
  */
 package byui.cit260.escapeIsland.view;
 
+import byui.cit260.escapeIsland.model.Player;
+import java.util.Scanner;
+
 /**
  *
  * @author Austin
@@ -12,7 +15,82 @@ package byui.cit260.escapeIsland.view;
 public class MainMenuView {
 
     public void displayMainMenuView() {
-        System.out.println("Display Main Menu View was called.");
+
+        boolean endOfView = false;
+        do {
+            String[] inputs = getInputs();
+            // System.out.println("input name");
+            
+            if (inputs[0].length() < 1 || inputs[0].equals('Q')) {
+                endOfView = true;   
+            }
+            endOfView = doAction(inputs);
+        } while (endOfView != true);
+
+    }
+
+    private String[] getInputs() {
+       String[] inputs = new String[1];
+
+        inputs = new String[1];
+
+        System.out.println("*****************"
+                          +"\n N - New Game  "
+                          +"\n L - Load Game "
+                          +"\n H - Help      "
+                          +"\n Q - Quit      "
+                          +"\n please select an option"
+                          +"\n***************");
+        
+
+        
+            String[] menuItem = new String[1];
+            Scanner sc = new Scanner(System.in);
+            menuItem[0] = sc.nextLine();
+           
+            
+            while(menuItem[0].toUpperCase().charAt(0) != 'N' 
+                    && menuItem[0].toUpperCase().charAt(0) != 'L' 
+                    && menuItem[0].toUpperCase().charAt(0) != 'H' 
+                    && menuItem[0].toUpperCase().charAt(0) !='Q' ){
+                         System.out.println("Enter a valid letter.");
+                         menuItem[0] = sc.nextLine();
+            }
+      return menuItem; 
+    } 
+
+    private boolean doAction(String[] inputs) {
+        
+    char c = inputs[0].trim().toUpperCase().charAt(0);
+        switch(c){
+            case 'N':
+                sng();
+                break;
+            case 'L':
+                lg();
+                break;
+            case 'H':
+                hm();
+                break;
+            case 'Q':
+                break;  
+        }
+        
+        return true;    
+
+    }
+
+    private void sng() {
+        System.out.println("Start a new game");
+    }
+
+    private void lg() {
+        System.out.println("Load a game");
+    }
+
+    private void hm() {
+        HelpMenuView help = new HelpMenuView();
+        help.displayHelpMenuView();
     }
     
 }
