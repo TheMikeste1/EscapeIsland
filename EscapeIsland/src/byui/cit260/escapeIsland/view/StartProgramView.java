23ee59cc96ba23d0448e5f46a4863a3215a094cb
Austi <Austi@LAPTOP-5ITOLKRT>
2018-06-12 13:02:01 -0600

    Merge origin/master
    Conflicts:
    	EscapeIsland/src/ModelLayer/Location.java
    	EscapeIsland/src/ModelLayer/PuzzleLocation.java

--- EscapeIsland/src/byui/cit260/escapeIsland/view/StartProgramView.java
+++ EscapeIsland/src/byui/cit260/escapeIsland/view/StartProgramView.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package byui.cit260.escapeIsland.view;
 
 import byui.cit260.escapeIsland.model.Player;
@@ -15,22 +10,27 @@ import java.util.Scanner;
 public class StartProgramView {
 
     private static String[] getInputs() {
-        System.out.println("**** getInputs() called ****");
+
         String[] inputs = new String[1];
 
         inputs = new String[1];
 
-        System.out.println("description of view");
-
+        System.out.println("***********************************"
+                + "\n*This is the game of Escape Island*"
+                + "\n*In this game you will escape from*"
+                + "\n*an island and try to understand  *"
+                + "\n*is happening.                    *"
+                + "\n***********************************");
         boolean valid = false;
+
         while (valid == false) {
-            System.out.println("prompt message");
-            String[] getName = new String[5];
+            System.out.println("Enter the player's name below:");
+            String[] getName = new String[1];
             Scanner sc = new Scanner(System.in);
             getName[0] = sc.nextLine();
-            System.out.println(getName[0]);
 
             String yourName = getName[0].trim();
+            System.out.println("");
             if (yourName.length() < 1) {
                 System.out.println("You must enter a non-blank value");
 
@@ -38,37 +38,38 @@ public class StartProgramView {
             }
             valid = true;
             inputs[0] = yourName;
-            inputs[0]=inputs[0].toUpperCase();
+            inputs[0] = inputs[0].toUpperCase();
         }
 
         return inputs;
     }
 
     private static boolean doAction(String[] inputs) {
-        
-String playersName = inputs[0];
-Player player = GameControl.savePlayer(playersName);
 
-if (player == null){
-    System.out.println("Could not create the player. " 
-                        + "Enter a diferent name.");
-    return false;   
-}
-        System.out.println("=================================================" 
-                         + "Welcome to the game " + playersName
-                         + "We hope you have a lot of fun!"
-                         + "=================================================");
-    
-   MainMenuView mainMenuView = new MainMenuView(); 
-   mainMenuView.displayMainMenuView();  
+        String playersName = inputs[0];
+        Player player = GameControl.savePlayer(playersName);
+
+        if (player == null) {
+            System.out.println("Could not create the player. "
+                    + "Enter a diferent name.");
+            return false;
+        }
+        System.out.println("================================================="
+                + " \nWelcome to the game " + playersName
+                + ". \nWe hope you have a lot of fun!"
+                + " \n=================================================");
+
+        MainMenuView mainMenuView = new MainMenuView();
+        mainMenuView.displayMainMenuView();
         return true;
     }
-    
+
     private static Player savePlayer(String playersName) {
-        return null;  
+        System.out.println("*** calling savePlayer ***");
+         return null;
     }
-    
-    public StartProgramView() {
+
+   public StartProgramView() {
 
     }
 
@@ -77,10 +78,10 @@ if (player == null){
         boolean endOfView = false;
         do {
             String[] inputs = getInputs();
-            System.out.println("input name");
-            
+            // System.out.println("input name");
+
             if (inputs[0].length() < 1 || inputs[0].equals('Q')) {
-                endOfView = true;   
+                endOfView = true;
             }
             endOfView = doAction(inputs);
         } while (endOfView != true);
