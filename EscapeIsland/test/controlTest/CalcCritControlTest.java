7c79f993253722ce038ca88c569f302e26054b2c
Collin <clarso24@byui.edu>
2018-06-13 13:45:22 -0600

    Merge origin/master
    Conflicts:
    	EscapeIsland/src/byui/cit260/escapeIsland/view/HelpMenuView.java

--- EscapeIsland/test/controlTest/CalcCritControlTest.java
+++ EscapeIsland/test/controlTest/CalcCritControlTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package citbyui.cit260.escapeIsland.controlTest;
+package controlTest;
 
 import control.CombatControl;
 import org.junit.Test;
