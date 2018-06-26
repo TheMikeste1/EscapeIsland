51e8dd601713716c04586b6911ab93650e654216
Collin <clarso24@byui.edu>
2018-06-26 13:01:41 -0600

    Merge origin/master
    Conflicts:
    	EscapeIsland/src/view/GameMenu.java

---
+++ EscapeIsland/src/control/CreateNewGame.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import model.Game;
+
+/**
+ *
+ * @author collin
+ */
+public class CreateNewGame {
+
+}
